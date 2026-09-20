a8a8632facfa7c884712e410ef1210eaefe1d832
ragul-creator <ragul.20070609@gmail.com>
2026-09-20 18:16:06 +0530

    H.2B.4: use type-only search form event import

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react";
+import { useState, type FormEvent } from "react";
 import { useQuery } from "@tanstack/react-query";
 import { Link, useParams } from "react-router-dom";
 import { searchProject } from "../api/platform";
